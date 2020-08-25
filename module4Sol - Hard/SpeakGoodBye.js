24c75efacbc593c4f26b5f3d9dc0e36bc277b174
FrootyEgg <aryan.gupta2018@vitbhopal.ac.in>
2020-08-26 00:40:17 +0530

    fixed an issue where random git stuff was written on all the module4 files

--- module4Sol - Hard/SpeakGoodBye.js
+++ module4Sol - Hard/SpeakGoodBye.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 (function (window) {
   var speakWord = "Good Bye";
   var byeSpeaker = {};
@@ -8,15 +7,4 @@
   }
 
   window.byeSpeaker = byeSpeaker;
-=======
-(function (window) {
-  var speakWord = "Good Bye";
-  var byeSpeaker = {};
-
-  byeSpeaker.speak = function (name) {
-    console.log(speakWord + " " + name);
-  }
-
-  window.byeSpeaker = byeSpeaker;
->>>>>>> 23a830134c85c0b9b7bd887b9a52efe8d49ddbb6
 })(window);
--- module4Sol - Hard/SpeakHello.js
+++ module4Sol - Hard/SpeakHello.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 (function (window) {
   var speakWord = "Hello";
   var helloSpeaker = {};
@@ -8,15 +7,4 @@
   }
 
   window.helloSpeaker = helloSpeaker;
-=======
-(function (window) {
-  var speakWord = "Hello";
-  var helloSpeaker = {};
-
-  helloSpeaker.speak = function (name) {
-    console.log(speakWord + " " + name);
-  }
-
-  window.helloSpeaker = helloSpeaker;
->>>>>>> 23a830134c85c0b9b7bd887b9a52efe8d49ddbb6
 })(window);
