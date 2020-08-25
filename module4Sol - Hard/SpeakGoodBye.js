dc808e4f54eee9b70ccb413aea9557c780f7fe00
FrootyEgg <aryan.gupta2018@vitbhopal.ac.in>
2020-08-26 00:31:50 +0530

    Module 4 - hard Solution

---
+++ module4Sol - Hard/SpeakGoodBye.js
@@ -0,0 +1,22 @@
+<<<<<<< HEAD
+(function (window) {
+  var speakWord = "Good Bye";
+  var byeSpeaker = {};
+
+  byeSpeaker.speak = function (name) {
+    console.log(speakWord + " " + name);
+  }
+
+  window.byeSpeaker = byeSpeaker;
+=======
+(function (window) {
+  var speakWord = "Good Bye";
+  var byeSpeaker = {};
+
+  byeSpeaker.speak = function (name) {
+    console.log(speakWord + " " + name);
+  }
+
+  window.byeSpeaker = byeSpeaker;
+>>>>>>> 23a830134c85c0b9b7bd887b9a52efe8d49ddbb6
+})(window);
---
+++ module4Sol - Hard/script.js
@@ -0,0 +1,29 @@
+<<<<<<< HEAD
+(function (window) {
+  var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+
+  for (var i in names) {
+    var firstLetter = names[i].charAt(0).toLowerCase();
+
+    if (firstLetter === 'j')
+      byeSpeaker.speak(names[i]);
+    else
+      helloSpeaker.speak(names[i]);
+  }
+})(window);
+
+=======
+(function (window) {
+  var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+
+  for (var i in names) {
+    var firstLetter = names[i].charAt(0).toLowerCase();
+
+    if (firstLetter === 'j')
+      byeSpeaker.speak(names[i]);
+    else
+      helloSpeaker.speak(names[i]);
+  }
+})(window);
+
+>>>>>>> 23a830134c85c0b9b7bd887b9a52efe8d49ddbb6
